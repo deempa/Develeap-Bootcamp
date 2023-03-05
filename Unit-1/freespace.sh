35f306be81e12ba25130b6e2ec65e2716a8ccf6c
Lior Rabanian <masrab11@gmail.com>
2023-03-05 15:16:57 +0200

    new changes

--- Unit-1/freespace.sh
+++ Unit-1/freespace.sh
@@ -22,12 +22,11 @@ function is_file_zipped ()
 {
     file_name=$1
     file_type=$(file $1 | cut -d' ' -f 2)
-
-    current_time=$(date +%s)
-    file_modified_time=$(stat -c %Y "$file_name")
+    echo $file_type
+    #timeout_in_seconds=$(($timeout_in_hours * 60))
 
     if [[ ${file_type} =~ [Zz][Ii][Pp] ]]; then
-        if [[ "$file_name" == fc-* && current_time - file_modified_time > 172800]]; then
+        if [ "$file_name" == fc-* ] && [ $(find "$file_name" -mtime +2 -print) ]; then
             rm "$file_name"
         fi
         return 0
@@ -38,55 +37,65 @@ function is_file_zipped ()
 
 function zip_directory_without_recursive ()
 {
-    
+    for f in $1/*
+    do
+        echo $f
+        zip_file $f
+    done
 }
 
 function zip_directory_with_recursive ()
 {
     for f in $1/*
-        do
-            if [[ $file_type == "directory" ]]; then
-                zip_directory_with_recursive $f
-            else
-                zip_file $f
-            fi
-        done
+    do
+        if [[ $file_type == "directory" ]]; then
+            zip_directory_with_recursive ${f}
+        else
+            zip_file ${f}
+        fi
+    done
 }
 
 function zip_file ()
 {
     is_file_zipped $1
     boolean_is_file_zipped=$?
-    echo $boolean_is_file_zipped
     if [[ $boolean_is_file_zipped -eq 1 ]]; then # if file is not compressed
-        zip "fc-${file_name}" ${file_name}
+        base_name=$(basename ${1})
+        base_name_with_fc="fc-${base_name}"
+        zip -d "fc-${base_name}" ${1}
     else
-        mv ${file_name} "fc-${file_name}"
-        touch "fc-${file_name}"
+        text_after_fc=$(echo $1 | cut -d'-' -f2-)
+        mv ${1} "fc-${text_after_fc}"
+        touch "fc-${text_after_fc}"
     fi
 }
 
 function start_zipping()
 {
     file_name=$1
-    file_type=$(file $1 | cut -d' ' -f 2)
+    echo $file_name
+    #file_full_path=$(realpath $file_name)
+    file_type=$(file $file_name | cut -d' ' -f 2)
     if [[ $recursive == true ]]; then
         if [[ $file_type == "directory" ]]; then
-            zip_directory_with_recursive ${file_name}
+            zip_directory_with_recursive $file_name
         else #is not directory
-            zip_file ${file_name}
+            zip_file $file_name
         fi
     else # recursive is false
         if [[ $file_type == "directory" ]]; then
-            zip_directory_without_recursive $f
+            echo $file_name
+            zip_directory_without_recursive $file_name
         else
-            zip_file ${file_name}
+            zip_file $file_name
         fi
     fi
 }
 
 for file_name in "$*"
 do
+    echo $file_name
     start_zipping $file_name
 done
 
